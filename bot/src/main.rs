d4f5b9484b5e7e1c5194c0f4651d4b44d2ac392f
Lamora <itsmerick184@gmail.com>
2025-05-26 04:36:42 +0700

    Update main.rs

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,6 +1,5 @@
 use teloxide::{prelude::*, dispatching::UpdateFilterExt, utils::command::BotCommands};
-use teloxide::types::ChatId;
-use bot::{admin::{AdminCommand, handle_command}, message::handle_message, database::Database};
+use crate::{admin::{AdminCommand, handle_command}, message::handle_message, database::Database};
 
 #[tokio::main]
 async fn main() {
@@ -8,27 +7,30 @@ async fn main() {
     pretty_env_logger::init();
     log::info!("Bot anti-gcast dimulai...");
 
-    // Init ML model sekali saat startup
-    ml::init_model().await;
 
     let bot = Bot::from_env();
     let db = Database::init().await;
 
     let db_message = db.clone();
 
-    Dispatcher::builder(bot.clone(), Update::filter_message())
+    let handler = dptree::entry()
         .branch(
-            dptree::entry()
+            Update::filter_message()
                 .filter_command::<AdminCommand>()
                 .endpoint(move |bot: Bot, msg: Message, cmd: AdminCommand| {
                     let db = db.clone();
                     async move { handle_command(bot, db, msg, cmd).await }
-                }),
+                })
         )
-        .default_handler(move |bot: Bot, msg: Message| {
-            let db = db_message.clone();
-            async move { handle_message(bot, db, msg).await.unwrap_or(()) }
-        })
+        .branch(
+            Update::filter_message()
+                .endpoint(move |bot: Bot, msg: Message| {
+                    let db = db_message.clone();
+                    async move { handle_message(bot, db, msg).await.unwrap_or(()) }
+                })
+        );
+
+    Dispatcher::builder(bot, handler)
         .enable_ctrlc_handler()
         .build()
         .dispatch()
