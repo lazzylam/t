872e9e98f79289198d8f514b0238554e63a42f21
Lamora <itsmerick184@gmail.com>
2025-05-26 03:49:42 +0700

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,43 +1,26 @@
+use serde::{Deserialize, Serialize};
 use mongodb::bson::oid::ObjectId;
-use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct BlacklistItem {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub group_id: i64,
+    pub keyword: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct WhitelistItem {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub group_id: i64,
+    pub keyword: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GroupSettings {
-    #[serde(rename = "_id")]
-    pub chat_id: i64,
-    pub is_active: bool,
-    pub updated_at: chrono::DateTime<chrono::Utc>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct BlacklistedText {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
-    pub chat_id: i64,
-    pub text_pattern: String,
-    pub created_at: chrono::DateTime<chrono::Utc>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct BlacklistedUser {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<ObjectId>,
-    pub chat_id: i64,
-    pub user_id: i64,
-    pub created_at: chrono::DateTime<chrono::Utc>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct WhitelistedUser {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<ObjectId>,
-    pub chat_id: i64,
-    pub user_id: i64,
-    pub created_at: chrono::DateTime<chrono::Utc>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SpamDetectionResult {
-    pub is_spam: bool,
-    pub reason: Option<String>,
+    pub group_id: i64,
+    pub enabled: bool,
 }
